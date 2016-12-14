8031c65f66d424d2a49d239ae69cd4a0c27315a1
liloman <eselilo@gmail.com>
2016-12-14 20:57:17 +0100

    *Fix regex to get the longer boot times

--- last-boots.sh
+++ last-boots.sh
@@ -8,7 +8,7 @@ last_boots() {
 #if nothing passed get last else all
 [[ -z $1 ]] && number=5 || number=0
 local com=(last -x --time-format iso -n $number)
-local shutdown='^shutdown system down  [-\.[:xdigit:][=x=]]* (.*)\+.* - (.*)\+.*  \((.*)\)' 
+local shutdown='^shutdown system down[[:space:]]+[-\.[:xdigit:][=x=]]* (.*)\+.* - (.*)\+.*\((.*)\)' 
 local from= to= duration= exec=
 
 while IFS= read -r line
@@ -17,7 +17,7 @@ do
         from=${BASH_REMATCH[1]} 
         to=${BASH_REMATCH[2]}
         duration=${BASH_REMATCH[3]}
-        exec="timew track from $from to $to Shutdown"
+        exec="timew track from $from - $to Shutdown"
         echo executing: $exec
         $exec
     fi
