24f1cff2d0c0d6f949ab117c7ace84d5508a514f
liloman <eselilo@gmail.com>
2017-02-08 18:09:46 +0100

    remove reminder and user add-reminder.sh from pomodoroTasks2 package

--- ptask.sh
+++ ptask.sh
@@ -115,64 +115,6 @@ newtema() {
     done
 }
 
-#Add a new reminder to taskwarrior
-reminder() {
-    local desc= 
-    local opt=
-    local recurrence=
-    local due wait_days default_days=15
-
-    echo "Select the type of reminder"
-    # select mul in "${opts[@]}";do
-    select mul in "Recurrent (ex: birthdate)" "NonRecurrent(from date)" "Inmediate(from today!)"; do
-        case $mul in
-            R*) opt=recurrent; break;;
-            N*) opt=nonrecurrent; break;;
-            I*) opt=inmediate; break;;
-        esac
-    done
-
-
-    read  -p "Description:"  desc
-
-    #if not inmediate
-    if [[ $opt != i* ]]; then
-        read  -p "Due date (dd/mm/yyyy). Empty for today:"  due
-        [[ -z $due ]] && due=today
-        local regex='^[0-3][0-9]/[0-1][0-9]/[0-9]{4}$'
-        if ! [[ $due =~ $regex || $due == today ]]; then
-            echo "due date: $due must have a valid format (dd/mm/yyyy)"; return
-        fi
-    fi
-
-    if [[ $opt == r* ]]; then
-        read  -p "Active n days before (empty for $default_days):"  wait_days
-        [[ -z $wait_days ]] && wait_days=$default_days
-        wait_days="due-${wait_days}d"
-        read  -p "Recurrence (empty for 1y):" recurrence 
-        [[ -z $recurrence ]] && recurrence=1years
-    elif [[ $opt == n* ]]; then
-        if [[ $due != today ]]; then
-            read  -p "Active n days before (empty for no wait):"  wait_days
-        fi
-        [[ -z $wait_days ]] && wait_days=today || wait_days="due-${wait_days}d"
-    else #inmediate so no due date
-        due=someday
-        wait_days=today
-    fi
-
-
-    local make_reminder="task add '$desc' pro:tasks due:$due wait:${wait_days} until:due+1d recur:$recurrence +reminder  rc.dateformat="D/M/Y""
-    echo "$make_reminder"
-    echo "Are you sure?"
-    select yn in Yes No; do
-        case $yn in
-            Y* ) $make_reminder; break;;
-            N* ) echo "Exit."; break;;
-        esac
-    done
-}
-
 #Set a notification on x time ahead
 notify() {
     local options=""
@@ -196,7 +138,6 @@ usage() {
     "  join        Join several tasks in one(wizard)"
     "  tema        Añade un nuevo tema a trabajar"
     "  notify      Add a temporal notification"
-    "  reminder    Add a reminder"
     "General options:"
     "  -h, --help  Show this help"
     "  -v          Show version"
@@ -224,9 +165,6 @@ while true; do
         tema)
             [[ -n $2 ]] && { echo "Option $1 needs no arguments (wizard)";exit 1; }
             newtema ; exit 0;;
-        reminder)
-            [[ -n $2 ]] && { echo "Option $1 needs no arguments (wizard)";exit 1; }
-            reminder ; exit 0;;
         notify)
             [[ -n $2 ]] && { echo "Option $1 needs no arguments (wizard)";exit 1; }
             notify ; exit 0;;
